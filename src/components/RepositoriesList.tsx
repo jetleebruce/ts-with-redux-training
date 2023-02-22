00d67478ea80b8753aeb8032f7889db877f83140
Modin Sergei <jetleebruce@gmail.com>
2023-02-22 15:15:22 +0300

    dispaly list of packages

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
-import { useSelector } from "react-redux";
 import { useActions } from "../hooks/useActions";
 
 const RepositoriesList: React.FC = () => {
@@ -26,6 +25,9 @@ const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
